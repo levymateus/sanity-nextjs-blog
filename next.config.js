3be58a5c495574118116416d927a1a1e90c3882e
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2023-04-12 21:58:21 -0300

    chore(next.config.js): change image domains for dev mode

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 const path = require('path')
 
+const imagesDomains = []
+const isDev = process.env.NODE_ENV === 'development' || process.env.NEXT_PUBLIC_ENV === 'development'
+
+if (isDev) {
+  imagesDomains.push('localhost')
+}
+
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  images: { domains: ["cdn.sanity.io", "localhost"] },
+  images: { domains: ["cdn.sanity.io", ...imagesDomains ] },
   reactStrictMode: true,
 }
 
