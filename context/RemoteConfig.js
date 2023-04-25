98444a6a804801de97aa34f745c9d217bfeeda70
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2023-04-24 21:42:22 -0300

    refactor(removeconfig.js): enable contact feat

--- context/RemoteConfig.js
+++ context/RemoteConfig.js
@@ -1,11 +1,11 @@
 import { createContext, useState } from "react"
 
 export const RemoteConfigContext = createContext({ config: {
-  contact: false
+  contact: true
 }})
 
 const RemoteConfig = ({ children }) => {
-  const [config] = useState({ contact: false })
+  const [config] = useState({ contact: true })
   return <RemoteConfigContext.Provider value={{ config }}>
     {children}
   </RemoteConfigContext.Provider>
