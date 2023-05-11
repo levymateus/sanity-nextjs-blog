922b10067a3721a5fc5953dfce9d7cf38e28047a
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2023-05-11 19:37:12 -0300

    refactor: remove unused hooks

--- hooks/useToggle.js
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from 'react'
-
-function useToggle(defaultValue = false) {
-  const [value, setValue] = useState(defaultValue)
-  const toggle = useCallback(() => setValue(value => !value), [])
-  return [value, toggle, setValue]
-}
-
-export default useToggle
