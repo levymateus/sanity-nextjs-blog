44eaf98cd9053eacb640e61aaa6f22fccfe1c71b
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2023-11-27 14:22:48 -0300

    perf: split home page code to better performance

---
+++ components/Contact.js
@@ -0,0 +1,9 @@
+import Card from "@components/Card"
+
+const Contact = () => {
+  return <section id="contact" className="pt-14">
+    <Card />
+  </section>
+}
+
+export default Contact
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Heading from "@components/Heading"
 import Text from "@components/Text"
 import Avatar from "@components/Avatar"
-import PostCard from "@components/PostCard"
-import Card from "@components/Card"
 import If from "@components/If"
-import Link from "next/link"
-import { ArrowRight } from "react-feather"
 import useConfig from "@hooks/useConfig"
 import client from "@utils/client"
 import { blurDataURL } from "@utils/blurDataURL"
+import dynamic from "next/dynamic"
 import '../envs'
+
+const FeaturedPosts = dynamic(() => import("../components/FeaturedPosts"))
+const Contact = dynamic(() => import("../components/Contact"))
 
 const HomePage = ({ bio, posts }) => {
   const { config } = useConfig()
@@ -36,39 +36,10 @@ const HomePage = ({ bio, posts }) => {
       </div>
     </section>
     <If stmt={posts.length}>
-      <section className="pt-14" >
-        <Heading size="xl" asChild>
-          <h3>Featured Posts</h3>
-        </Heading>
-        <div className="flex flex-col sm:flex-row space-y-6 sm:space-y-0 sm:space-x-7 mt-6 fade-in">
-          <PostCard
-            slug={posts[0]?.slug.current}
-            description={posts[0]?.title} color="teal"
-          />
-          <If stmt={posts.length >= 2}>
-            <PostCard
-              slug={posts[1]?.slug.current}
-              description={posts[1]?.title}
-              color="indigo"
-            />
-          </If>
-          <If stmt={posts.length >= 3}>
-            <PostCard
-              slug={posts[2]?.slug.current}
-              description={posts[2]?.title}
-              color="purple"
-            />
-          </If>
-        </div>
-        <Link className="text-neutral-500" href="/blog" passHref>
-          <div className="flex items-center space-y-1 pt-6 pb-6 text-black hover:text-blue-600 dark:hover:text-white text-md dark:text-neutral-500">Read all posts <ArrowRight width={14} height={14} /></div>
-        </Link>
-      </section>
+      <FeaturedPosts posts={posts} />
     </If>
     <If stmt={config?.contact}>
-      <section id="contact" className="pt-14">
-        <Card />
-      </section>
+      <Contact />
     </If>
   </>
 };
