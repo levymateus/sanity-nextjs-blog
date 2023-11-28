f7bb6c952840f792e49874a92481f9962c57a15c
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2023-11-28 16:13:29 -0300

    chore: next upgrade from 13 to 14

--- next.config.js
+++ next.config.js
@@ -2,17 +2,14 @@ const path = require('path')
 const withBundleAnalyzer = require('@next/bundle-analyzer')
 
 const isBundleAnalyzeEnabled = process.env.ANALYZE == 'true'
-const imagesDomains = []
-
-if (process.env.NEXT_PUBLIC_ENV == process.env.NODE_ENV) {
-  imagesDomains.push('localhost')
-}
 
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  images: { domains: ["cdn.sanity.io", ...imagesDomains ] },
+  images: {
+    domains: ["cdn.sanity.io"]
+  },
   reactStrictMode: true,
 }
 
