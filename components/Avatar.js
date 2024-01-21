a04db7d85a3104bc021cf12891c150d5edc87a50
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2024-01-20 22:37:30 -0300

    fix: avatar sizing (#16)

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const Avatar = ({ className, src, alt, ...props }) => {
   return <Image
-    className={clsx(className, "rounded-full h-32")}
+    className={clsx(className, "rounded-full h-32 object-cover")}
     width={128}
     height={128}
     src={src}
